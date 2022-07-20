920841a7001599b1d58be9c8f34e59e29f884f47
Felipe Tarran Veronezzi <82220017+felipetarran@users.noreply.github.com>
2022-07-20 18:54:38 -0300

    Create reindexer.sh

---
+++ reindexer.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+curl -o indexes.json http://${SRC_SERVER_IP}:9200/_aliases?pretty=true
+
+INDEXES=$(jq -r 'keys[]' indexes.json)
+
+echo ${INDEXES[*]}  
+
+
+for index in ${INDEXES[@]}; do
+	curl -X POST "${TARGET_SERVER_IP}:9200/_reindex?wait_for_completion=true&pretty=true" -H 'Content-Type: application/json' -d'
+	{
+		"source": {
+			"remote": {
+				"host": "http://${SRC_SERVER_IP}:9200"
+			},
+			"index": "'$index'"
+		},
+		"dest": {
+			"index": "'$index'"
+		}
+	}'
+done
